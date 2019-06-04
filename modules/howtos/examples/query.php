693db018c31cf6892f78bfd9b8c7438da44270f8
Charles Dixon <chvckd@gmail.com>
2019-06-04 11:10:39 +0100

    Update n1ql page for 3.0

---
+++ modules/howtos/examples/query.php
@@ -0,0 +1,33 @@
+<?php
+$connectionString = "couchbase://10.112.193.101";
+$cluster = new \Couchbase\Cluster($connectionString);
+
+$cluster->authenticateAs("Administrator", "password");
+$bucket = $cluster->bucket("travel-sample");
+$collection = $bucket->defaultCollection();
+
+// #tag::positionalParams[]
+$options = new \Couchbase\QueryOptions();
+$options->positionalParameters(["hotel"]);
+$result = $bucket->query('SELECT x.* FROM `travel-sample` x WHERE x.`type`=$1 LIMIT 10;', $options);
+// #end::positionalParams[]
+
+// #tag::namedParams[]
+$options = new \Couchbase\QueryOptions();
+$options->namedParameters(['type' => "hotel"]);
+$result = $bucket->query('SELECT x.* FROM `travel-sample` x WHERE x.`type`=$type LIMIT 10;', $options);
+// #end::namedParams[]
+
+// #tag::results[]
+$options = new \Couchbase\QueryOptions();
+$options->positionalParameters(["hotel"]);
+$result = $bucket->query('SELECT x.* FROM `travel-sample` x WHERE x.`type`=$1 LIMIT 10;', $options);
+
+foreach($result->rows() as $row) {
+    printf("Name: %s, Address: %s, Description: %s\n", $row["name"], $row["address"], $row["description"]);
+}
+// #end::results[]
+
+// #tag::scan[]
+// TODO: update when consistency is implemented
+// #end::scan[]
