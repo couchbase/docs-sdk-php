c6bee70b351edae633e45052d8f95b448c646d0f
Charles Dixon <chvckd@gmail.com>
2019-07-17 09:02:38 +0100

    Add analytics docs

---
+++ modules/howtos/examples/analytics.php
@@ -0,0 +1,59 @@
+<?php
+$connectionString = "couchbase://10.112.193.101";
+$cluster = new \Couchbase\Cluster($connectionString);
+
+$cluster->authenticateAs("Administrator", "password");
+$bucket = $cluster->bucket("travel-sample");
+$collection = $bucket->defaultCollection();
+
+// #tag::query[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$result = $cluster->analyticsQuery('SELECT "hello" as greeting;', $options);
+
+foreach($result->rows() as $row) {
+    printf("result: %s\n", $row["greeting"]);
+}
+// #end::query[]
+
+// #tag::simple[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$result = $cluster->analyticsQuery('SELECT airportname, country FROM airports WHERE country = "France";', $options);
+// #end::simple[]
+
+// #tag::positional[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$options->positionalParameters(["France"]);
+$result = $cluster->analyticsQuery('SELECT airportname, country FROM airports WHERE country = $1;', $options);
+// #end::positional[]
+
+// #tag::named[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$options->namedParameters(['$country' => "France"]);
+$result = $cluster->analyticsQuery('SELECT airportname, country FROM airports WHERE country = $country;', $options);
+// #end::named[]
+
+ // #tag::options[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$options->timeout(100);
+$result = $cluster->analyticsQuery('SELECT airportname, country FROM airports WHERE country = "France";', $options);
+// #end::options[]
+
+// #tag::results[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$result = $cluster->analyticsQuery('SELECT airportname, country FROM airports WHERE country = "France";', $options);
+
+foreach($result->rows() as $row) {
+printf("Name: %s, Country: %s\n", $row["name"], $row["country"]);
+}
+// #end::results[]
+
+// #tag::metadata[]
+$options = new \Couchbase\AnalyticsQueryOptions();
+$result = $cluster->analyticsQuery('SELECT airportname, country FROM airports WHERE country = "France";', $options);
+
+$metadata = $result->metadata();
+$metrics = $metadata->metrics();
+printf("Elapsed time: %d\n", $metrics["elapsedTime"]);
+printf("Execution time: %d\n", $metrics["executionTime"]);
+printf("Result count: %d\n", $metrics["resultCount"]);
+// #tag::metadata[]
