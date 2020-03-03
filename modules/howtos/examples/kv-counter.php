b7588318298e19182c8dcd2d94bf4af3d2c5f453
Richard Smedley <rgsmedley@gmail.com>
2020-03-03 18:35:03 +0000

    : --> ::

--- modules/howtos/examples/kv-counter.php
+++ modules/howtos/examples/kv-counter.php
@@ -12,21 +12,21 @@ $bucket = $cluster->bucket("travel-sample");
 $collection = $bucket->defaultCollection();
 
 $collection->upsert("foo", 0);
-// #tag:increment[]
+// #tag::increment[]
 // increment binary value by 1 (default)
 $binaryCollection = $collection->binary();
 $res = $binaryCollection->increment("foo");
-// #end:increment[]
+// #end::increment[]
 
-// #tag:decrement[]
+// #tag::decrement[]
 // decremtnt binary value by 1 (default)
 $res = $binaryCollection->decrement("foo");
-// #end:decrement[]
+// #end::decrement[]
 
 // Increment & Decrement are considered part of the 'binary' API
 // and as such may still be a subject to change
 
-// #tag:incrementwithoptions[]
+// #tag::incrementwithoptions[]
 // Create a document and assign it to 10 -- counter works atomically
 // by first creating a document if it doesn't exist. If it exists,
 // the same method will increment/decrement per the "delta" parameter
@@ -37,21 +37,21 @@ $opts->initial(10)->delta(2);
 $res = $binaryCollection->increment($key, $opts);
 // Should print 10
 printf("Initialized Counter: %d\n", $res->content());
-// #end:incrementwithoptions[]
+// #end::incrementwithoptions[]
 
 // Issue the same operation, increment value by 2 to 12
 $res = $binaryCollection->increment($key, $opts);
 // Should print 12
 printf("Incremented Counter: %d\n", $res->content());
 
-// #tag:decrementwithoptions[]
+// #tag::decrementwithoptions[]
 $opts = new DecrementOptions();
 $opts->initial(10)->delta(4);
 // Decrement value by 4 to 8
 $res = $binaryCollection->decrement($key, $opts);
 // Should print 8
 printf("Decremented Counter: %d\n", $res->content());
-// #end:decrementwithoptions[]
+// #end::decrementwithoptions[]
 
 // Output:
 //
