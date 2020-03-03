86e2e8f115aefea3f5944eee9b226b7b90c40695
Richard Smedley <rgsmedley@gmail.com>
2020-03-03 18:09:46 +0000

    : --> ::

--- modules/howtos/examples/durability.php
+++ modules/howtos/examples/durability.php
@@ -10,11 +10,11 @@ $cluster = new Cluster("couchbase://192.168.1.101", $opts);
 
 $collection = $cluster->bucket("travel-sample")->defaultCollection();
 
-// #tag:enhanced[]
+// #tag::enhanced[]
 $document = ["foo" => "bar", "bar" => "foo"];
 // Upsert with Durability level Majority
 $opts = new UpsertOptions();
 $opts->durabilityLevel(DurabilityLevel::MAJORITY);
 $res = $collection->upsert("durable-key", $document, $opts);
 printf("Successfully created document \"durable-key\". CAS=\"%s\"\n", $res->cas());
-// #end:enhanced[]
+// #end::enhanced[]
