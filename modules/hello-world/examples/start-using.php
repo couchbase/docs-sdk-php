eca7d3fd03092312e2e44c197ca53e1fb536b5b7
Hakim Cassimally <hakim.cassimally@couchbase.com>
2022-05-02 14:27:22 +0100

    DOC-9823 PHP start-using Capella first examples

---
+++ modules/hello-world/examples/start-using.php
@@ -0,0 +1,45 @@
+<?php
+
+// tag::imports[]
+require_once 'Couchbase/autoload.php';
+
+use \Couchbase\ClusterOptions;
+use \Couchbase\Cluster;
+// end::imports[]
+
+// tag::connect[]
+// Update these credentials for your Local instance!
+$connectionString = "couchbase://db";
+$options = new \Couchbase\ClusterOptions();
+
+$options->credentials("username", "Password!123");
+$cluster = new \Couchbase\Cluster($connectionString, $options);
+// end::connect[]
+
+// tag::bucket[]
+// get a bucket reference
+$bucket = $cluster->bucket("travel-sample");
+// end::bucket[]
+
+// tag::collection[]
+// get a user-defined collection reference$scope = $bucket->scope("tenant_agent_00");
+$scope = $bucket->scope("tenant_agent_00");
+$collection = $scope->collection("users");
+// end::collection[]
+
+// tag::upsert-get[]
+$upsertResult = $collection->upsert("my-document-key", ["name" => "Ted", "Age" => 31]);
+
+$getResult = $collection->get("my-document-key");
+
+print_r($getResult->content());
+// end::upsert-get[]
+
+// tag::n1ql-query[]
+$queryResult = $cluster->query("select \"Hello World\" as greeting");
+
+// Iterate over the rows to access result data and print to the terminal.
+foreach ($queryResult->rows() as $row) {
+    printf("%s\n", $row["greeting"]);
+}
+// end::n1ql-query[]
